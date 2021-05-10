be0d047fc559eae3c9759d80102e5c25fd69a727
Gennadii Marianychenko <argent.genesis@gmail.com>
2021-05-10 13:28:16 +0300

    add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conans import ConanFile, CMake
+from conans.tools import replace_in_file
+
+class sqlpp11Conan(ConanFile):
+    name = 'sqlpp11-connector-mysql'
+    version = '1.0'
+    license = 'BSD'
+    author = 'Gennadii Marianychenko<argent.genesis@gmail.com>'
+    url = 'https://github.com/ggeenn/conan-sqlpp11-connector-mysql.git'
+    description = 'Conan recipie for rbock/sqlpp11-connector-mysql'
+    generators = 'cmake'
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    short_paths = True
+    requires = "sqlpp11/0.60", "date/3.0.0", "libmysqlclient/8.0.17", "openssl/1.1.1k"
+
+    def source(self):
+        self.run('git clone https://github.com/rbock/sqlpp11')
+        self.run('git clone https://github.com/rbock/sqlpp11-connector-mysql')
+        self.run('git clone https://github.com/howardhinnant/date')
+        replace_in_file(
+            'sqlpp11-connector-mysql/CMakeLists.txt',
+            'project (sqlpp11-connector-mysql)',
+            '''
+project (sqlpp11-connector-mysql)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()
+'''
+        )
+        replace_in_file('sqlpp11-connector-mysql/CMakeLists.txt',
+                        'add_subdirectory(tests)', '')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="sqlpp11-connector-mysql")
+        cmake.build()
+
+    def package(self):
+        self.copy('*', dst='scripts', src='sqlpp11/scripts')
+        self.copy('*.h', dst='include/sqlpp11', src='sqlpp11-connector-mysql/include/sqlpp11')
+        self.copy('*.lib', dst='lib', src='sqlpp11-connector-mysql', keep_path=False)
+        self.copy('*.a', dst='lib', src='sqlpp11-connector-mysql', keep_path=False)
+        self.copy('*.so*', dst='lib', src='sqlpp11-connector-mysql', keep_path=False)
+        self.copy('*.so*', dst='bin', src='sqlpp11-connector-mysql', keep_path=False)
+
+
+    def package_info(self):
+        self.cpp_info.libs = ['sqlpp-mysql']
